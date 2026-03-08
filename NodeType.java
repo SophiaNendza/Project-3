d49880c27ca36599abb41ebe79359c51a06ed318
KLane2002 <KaseyLane2002@gmail.com>
2026-03-08 08:56:00 -0400

    Update NodeType.java

--- NodeType.java
+++ NodeType.java
@@ -1,4 +1,11 @@
-public class NodeType<T extends Comparable<T>>  {
-    public NodeType<T> left = null;
-    public NodeType<T> right = null;
-} // NodeType
+public class NodeType<T extends Comparable<T>> {
+    public T info;
+    public NodeType<T> left;
+    public NodeType<T> right;
+
+    public NodeType(T info) {
+        this.info = info;
+        left = null;
+        right = null;
+    }
+}
